3ca5dedd9fa3b4badf8e060a735e3689391aa8b9
Dhanveer Singh <singhdhanveer007@gmail.com>
2023-04-11 21:11:17 +0530

    Episod 3- Laying the Foundation

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "Hello I am h1 Tag")))
+
+const Title = () => {
+    return <h1 className="head">This is Title of the page</h1>
+}
+const Heading = () => {
+    return (
+        <div id="container">
+            <Title />
+            <h1>Namaste React from Functional Component 🚀</h1>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent)
+root.render(<Heading />)
